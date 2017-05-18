85db5ac9533f8be9601ae37ecc4441ed2c55c24a
Shaun <seyyes2000@hotmail.com>
2017-05-18 15:00:59 -0400

    finish multiples2

--- multiples_3_5.py
+++ multiples_3_5.py
@@ -1,19 +1,46 @@
-
 def multiples(n):
-	total = 0
-	for num in range(0, n, 3):
-		total += num
 
-	for number in range(0, n, 5):
-		if number % 3 == 0:
-			continue
-		else:
-			total += number
-		# if num % 3 == 0 or num % 5 == 0:
-
-	print (total)
+	print (sum(set(list(range(3, n, 3)) + list(range(5, n, 5)))))
 
 print (multiples(10))
+
+
+
+# def multiples(n):
+
+# 	first_sum = sum(range(3, n, 3))
+
+# 	return first_sum + sum(x for x in range(5, n, 5) if x % 3)
+
+# 	# # for number in range(0, n, 5):
+# 	# 	if number % 3 == 0:
+# 	# 		continue
+# 	# 	else:
+# 	# 		total += number
+# 	# 	# if num % 3 == 0 or num % 5 == 0:
+
+# 	# print (total)
+
+# print (multiples(10))
+
+
+
+
+# def multiples(n):
+# 	total = 0
+# 	for num in range(0, n, 3):
+# 		total += num
+
+# 	for number in range(0, n, 5):
+# 		if number % 3 == 0:
+# 			continue
+# 		else:
+# 			total += number
+# 		# if num % 3 == 0 or num % 5 == 0:
+
+# 	print (total)
+
+# print (multiples(10))
 
 
 
